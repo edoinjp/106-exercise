0fd7a43c50c1231738c3b82b2096e422c3c38347
eduard gurchiani <edoinjp@gmail.com>
2026-04-09 19:39:16 +0400

    resistorColor

---
+++ src/resistorColor.ts
@@ -0,0 +1,31 @@
+let colors: string[] = [
+  "black",
+  "brown",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "grey",
+  "white",
+];
+
+function find(color:string) {
+  return colors.indexOf(color)
+}
+
+console.log(colors);
+console.log(find('brown'))
+
+
+// black: 0;
+// brown: 1;
+// red: 2;
+// orange: 3;
+// yellow: 4;
+// green: 5;
+// blue: 6;
+// violet: 7;
+// grey: 8;
+// white: 9;
