8e8007f6a29bafa31a3000d91f45d21357069258
eduard gurchiani <edoinjp@gmail.com>
2026-04-24 12:04:25 +0400

    roman numerals

---
+++ src/romanNumerals.ts
@@ -0,0 +1,16 @@
+export function toRoman(n: number): string {
+  const map: [number, string][] = [
+    [1000, "M"], [900, "CM"], [500, "D"], [400, "CD"],
+    [100, "C"], [90, "XC"], [50, "L"], [40, "XL"],
+    [10, "X"], [9, "IX"], [5, "V"], [4, "IV"], [1, "I"]
+  ];
+
+  let result = "";
+  for (const [val, symbol] of map) {
+    while (n >= val) {
+      result += symbol;
+      n -= val;
+    }
+  }
+  return result;
+}
